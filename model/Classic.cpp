f901c5091b0493629b5678f29636d651f16ece54
bigcubecat <bigcubecat@arch.localdomain>
2020-12-07 01:31:37 +0000

    fixed backend (may be)

---
+++ model/Classic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bigcubecat on 03.12.2020.
+//
+
+#include "classic.h"
---
+++ model/Classic.h
@@ -0,0 +1,14 @@
+//
+// Created by bigcubecat on 03.12.2020.
+//
+
+#ifndef GAMEOFLIFE_CLASSIC_H
+#define GAMEOFLIFE_CLASSIC_H
+
+
+class classic {
+
+};
+
+
+#endif //GAMEOFLIFE_CLASSIC_H
