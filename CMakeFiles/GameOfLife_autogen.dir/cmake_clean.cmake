cf73a9b6853d7ceb2f3758f70c4cb000e4eba190
bigcubecat <mr.bitochkin@yandex.ru>
2021-01-31 14:38:39 +0700

    clear repo

--- CMakeFiles/GameOfLife_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GameOfLife_autogen"
-  "GameOfLife_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GameOfLife_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
